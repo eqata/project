c603a4581c92ccef6cdfd1968b7d2655c1a82304
eqata <eqataqq@gmail.com>
2026-09-20 20:12:34 +0300

    лаба1

---
+++ lab1++/medium1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <iomanip>
+
+int main() {
+    int mark1, mark2, mark3;
+
+    std::cout << "Введите первую оценку: ";
+    // проверка на дробное число
+    if (!(std::cin >> mark1) || std::cin.peek() == '.') {
+        std::cout << "Ошибка: введено дробное число или буквы." << std::endl;
+        return 1;
+    }
+
+    std::cout << "Введите вторую оценку: ";
+    if (!(std::cin >> mark2) || std::cin.peek() == '.') {
+        std::cout << "Ошибка: введено дробное число или буквы." << std::endl;
+        return 1;
+    }
+
+    std::cout << "Введите третью оценку: ";
+    if (!(std::cin >> mark3) || std::cin.peek() == '.') {
+        std::cout << "Ошибка: введено дробное число или буквы." << std::endl;
+        return 1;
+    }
+
+    // вычисление среднего балла
+    double result = (mark1 + mark2 + mark3) / 3.0;
+
+    // настройка вывода и вывод
+    std::cout << std::fixed << std::setprecision(2);
+    std::cout << "Средний балл: " << result << std::endl;
+
+    return 0;
+    }
